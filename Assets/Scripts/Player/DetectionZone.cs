16e70d4dbc4c08846e8d76835ced0b945edf5ac3
Quentin <bm.quentin@icloud.com>
2022-04-05 02:27:32 +0200

    code

--- Assets/Scripts/Player/DetectionZone.cs
+++ Assets/Scripts/Player/DetectionZone.cs
@@ -85,7 +85,9 @@ public class DetectionZone : MonoBehaviour
     private void DetectTank()
     {
 
-        //ContactFilter2D contactfliter = contactfliter.SetLayerMask();
-       // Physics2D.OverlapBox();
+        ContactFilter2D contactfliter = new ContactFilter2D();
+        contactfliter.SetLayerMask(layerMask);
+
+        Physics2D.OverlapBox()
     }
 }
