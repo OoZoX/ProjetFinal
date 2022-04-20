48b3e3ee9445d701214436b024929a35a5eb9bac
Quentin <bm.quentin@icloud.com>
2022-04-20 15:27:52 +0200

    move + import flow field

--- Assets/Scripts/Mouvement/MouvementTank.cs
+++ Assets/Scripts/Mouvement/MouvementTank.cs
@@ -35,10 +35,10 @@ public class MouvementTank : MonoBehaviour
         Debug.Log("<color=green>" + _cible + "</color>");
         ManagerGraph.Instance.m_StartParcourChemin(_cible);
 
-        TraceCheminFlowFild(ManagerGraph.Instance.m_listCaseMap);
+        TraceCheminFlowFild(ManagerGraph.Instance.m_tabCellMap);
     }
 
-    private void TraceCheminFlowFild(Case[,] AllCaseMap)
+    private void TraceCheminFlowFild(Cell[,] AllCaseMap)
     {
         foreach (var tank in SelectionTank.Instance.m_collider2DsTank)
         {
@@ -48,7 +48,7 @@ public class MouvementTank : MonoBehaviour
         }
     }
 
-    private IEnumerator CalculeChemin(Collider2D tank, Case[,] AllCaseMap)
+    private IEnumerator CalculeChemin(Collider2D tank, Cell[,] AllCaseMap)
     {
         Vector2 posTank = new Vector2(
                                             (int)tank.transform.position.x - ManagerGraph.Instance.m_positionStart_X,
@@ -56,9 +56,9 @@ public class MouvementTank : MonoBehaviour
                                          );
 
 
-        Case LastCase;
+        Cell LastCase;
 
-        tank.GetComponent<PlayerTank>().m_cheminDep.Add(AllCaseMap[(int)posTank.x, (int)posTank.y].m_pos);
+        tank.GetComponent<PlayerTank>().m_cheminDep.Add(AllCaseMap[(int)posTank.x, (int)posTank.y].m_posWorld);
         LastCase = AllCaseMap[(int)posTank.x, (int)posTank.y];
         int DistanceCible = LastCase.m_distance; 
 
@@ -67,7 +67,7 @@ public class MouvementTank : MonoBehaviour
             tank.GetComponent<PlayerTank>().m_cheminDep.Add(AllCaseMap[
                                                                         (int)LastCase.m_parentSearch.x,
                                                                         (int)LastCase.m_parentSearch.y
-                                                                      ].m_pos);
+                                                                      ].m_posWorld);
 
             LastCase = AllCaseMap[
                                     (int)LastCase.m_parentSearch.x,
---
+++ Assets/Scripts/script ext/CellCopy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CellCopy
+{
+	public Vector3 worldPos;
+	public Vector2Int gridIndex;
+	public byte cost;
+	public ushort bestCost;
+	public GridDirection bestDirection;
+
+	public CellCopy(Vector3 _worldPos, Vector2Int _gridIndex)
+	{
+		worldPos = _worldPos;
+		gridIndex = _gridIndex;
+		cost = 1;
+		bestCost = ushort.MaxValue;
+		bestDirection = GridDirection.None;
+	}
+
+	public void IncreaseCost(int amnt)
+	{
+		if (cost == byte.MaxValue) { return; }
+		if (amnt + cost >= 255) { cost = byte.MaxValue; }
+		else { cost += (byte)amnt; }
+	}
+}
