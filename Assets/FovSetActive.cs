dee69ce028e597ab452bbba165ee6a22b19da4d8
Quentin <bm.quentin@icloud.com>
2022-04-21 18:58:27 +0200

    FOV

--- Assets/FovSetActive.cs
+++ Assets/FovSetActive.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.UI;
 
 public class FovSetActive : MonoBehaviour
 {
@@ -31,6 +32,10 @@ public class FovSetActive : MonoBehaviour
             if (collider.gameObject.CompareTag("Ennemy"))
             {
                 collider.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+                collider.gameObject.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().enabled = true;
+                collider.gameObject.GetComponent<IATank>()._UITank.transform.GetChild(0).gameObject.SetActive(true);
+                collider.gameObject.GetComponent<IATank>()._UITank.transform.GetChild(1).GetChild(0).gameObject.GetComponent<Image>().enabled = true;
+                collider.gameObject.GetComponent<IATank>()._UITank.transform.GetChild(1).GetChild(1).gameObject.GetComponent<Image>().enabled = true;
                 _colliderVisible.Add(collider);
             }
         }
@@ -40,6 +45,10 @@ public class FovSetActive : MonoBehaviour
             if (!_colliderList.Contains(collider))
             {
                 collider.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+                collider.gameObject.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().enabled = false;
+                collider.gameObject.GetComponent<IATank>()._UITank.transform.GetChild(0).gameObject.SetActive(false);
+                collider.gameObject.GetComponent<IATank>()._UITank.transform.GetChild(1).GetChild(0).gameObject.GetComponent<Image>().enabled = false;
+                collider.gameObject.GetComponent<IATank>()._UITank.transform.GetChild(1).GetChild(1).gameObject.GetComponent<Image>().enabled = false;
                 _colliderVisible.Remove(collider);
             }
         }
